75873fba73b08447f85d0f270850bb671ef3fc41
Mounir-abaddah <mounirabaddah6@gmail.com>
2025-09-20 14:50:48 +0100

    :sparkles: feat: configuration du serveur Express avec Passport et routes d'authentification

--- server/index.js
+++ server/index.js
@@ -1,1 +1,33 @@
-const express = require('express')
+const express = require('express');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const passport = require('passport');
+require('dotenv').config();
+require('./config/dbConfig');
+require('./config/passport');
+
+const authRoute = require('./routes/authRoute');
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.use(
+  session({
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/auth', authRoute);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
